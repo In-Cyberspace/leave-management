c896294d92315d977c45be4fa8c3d7c020e4a386
Hugo Molaudzi <57953645+In-Cyberspace@users.noreply.github.com>
2020-05-30 13:19:10 +0200

    Implemented a data seeder to seed the database with the "Administrator" and "Employee" roles as well as a default admin user. The asynchronous post method of the register model now automatically assigns the employee role to each new user during the registration process.

---
+++ SeedData.cs
@@ -0,0 +1,58 @@
+﻿using leave_management.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace leave_management
+{
+    public static class SeedData
+    {
+        public static void Seed(UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            SeedRoles(roleManager);
+            SeedUsers(userManager);
+        }
+
+        private static void SeedUsers(UserManager<IdentityUser> userManager)
+        {
+            if (userManager.FindByNameAsync("admin").Result == null)
+            {
+                // Create "admin" user if it does not exist.
+                Employee user = new Employee
+                {
+                    UserName = "admin",
+                    Email = "admin@localhost.com"
+                };
+                
+                // Assign the "Administrator" role to the newly created "admin" user.
+                IdentityResult result = userManager.CreateAsync(user, "P@ssw0rd").Result;
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Administrator").Wait();
+                }
+            }
+        }
+
+        private static void SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+            if (!roleManager.RoleExistsAsync("Administrator").Result)
+            {
+                IdentityRole role = new IdentityRole
+                {
+                    Name = "Administrator"
+                };
+                roleManager.CreateAsync(role).Wait();
+                // IdentityResult result = roleManager.CreateAsync(role).Result;
+            }
+
+            if (!roleManager.RoleExistsAsync("Employee").Result)
+            {
+                IdentityRole role = new IdentityRole
+                {
+                    Name = "Employee"
+                };
+                roleManager.CreateAsync(role).Wait();
+                // IdentityResult result = roleManager.CreateAsync(role).Result;
+            }
+        }
+    }
+}
