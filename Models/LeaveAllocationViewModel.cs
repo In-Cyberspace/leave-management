37857cab7391d1594b700216a99ee6f2d90af2dc
Hugo Molaudzi <57953645+In-Cyberspace@users.noreply.github.com>
2020-06-14 19:56:37 +0200

    Leave types Create, Details, Edit, Index Views: Implemented the default days property into these views.
    Shared layout: Linked the leave allocations dropdown to the leave
    allocations index page.
    
    Leave allocation repository: Added the "check allocation" method to the
    contract and consequently the repository. This method confirms whether
    or not the respective allocation has already been applied to the
    existing employees.
    
    Leave allocation view model: Added the "create leave allocation" view
    model.
    
    Leave allocation controller and view: Scaffolded the leave allocations
    controller. The index method has been modified and the respective view
    has been scaffolded and modified. Moreover, the "set leave" method has
    been created within the controller.

--- Models/LeaveAllocationViewModel.cs
+++ Models/LeaveAllocationViewModel.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace leave_management.Models
@@ -11,58 +10,69 @@ namespace leave_management.Models
     public class LeaveAllocationViewModel
     {
         /// <summary>
-        /// Unique identifier for the leave allocation.
+        /// Gets or sets the unique identifier for the leave allocation.
         /// </summary>
         public int Id { get; set; }
 
         /// <summary>
-        /// Represents the number of days allocated to the leave type.
+        /// Gets or sets the number of days allocated to the leave type.
         /// </summary>
         public int NumberOfDays { get; set; }
 
         /// <summary>
-        /// Represents the date the allocations were made. This date corresponds
-        /// to the date the leave type was created.
+        /// Gets or sets the date that the date of creation of the leave 
+        /// allocation.
         /// </summary>
         public DateTime DateCreated { get; set; }
 
         /// <summary>
-        /// The period/year during which the leave type applies.
+        /// Gets or sets the period/year during which the leave type applies.
         /// </summary>
         public int Period { get; set; }
 
         /// <summary>
-        /// Represents the employee to whom the allocation are being made.
+        /// Gets or sets the employee associated with the leave allocation.
         /// </summary>
         public EmployeeViewModel Employee { get; set; }
 
         /// <summary>
-        /// The unique identifier for the employee associated with the
-        /// allocation.
+        /// Gets or sets the unique identifier for the employee associated with
+        /// the allocation.
         /// </summary>
         public string EmployeeId { get; set; }
 
         /// <summary>
-        /// Represents the type of leave to the allocation is associated with.
+        /// Gets or sets the leave type being allocated.
         /// </summary>
         public LeaveTypeViewModel LeaveType { get; set; }
 
         /// <summary>
-        /// The unique identifier for the leave type associated with the
-        /// allocation.
+        /// Gets or sets the unique identifier for the allocated leave type.
         /// </summary>
         public int LeaveTypeId { get; set; }
 
         /// <summary>
-        /// Represents a collection of employees in the database. This
-        /// collection is to be displayed using a drop down list.
+        /// Gets or sets a collection of registered employees.
         /// </summary>
         public IEnumerable<SelectListItem> Employees { get; set; }
 
         /// <summary>
-        /// Represents a collection of leave types in the database. This
-        /// collection is to be displayed using a drop down list.
+        /// Gets or sets a collection of available leave types.
         /// </summary>
         public IEnumerable<SelectListItem> LeaveTypes { get; set; }
+    }
+
+    public class CreateLeaveAllocationViewModel
+    {
+        /// <summary>
+        /// Gets or sets the number of records that were updated by the
+        /// parent method.
+        /// </summary>
+        public int NumberUpdated { get; set; }
+
+        /// <summary>
+        /// Gets or sets a list of leave types.
+        /// </summary>
+        public List<LeaveTypeViewModel> LeaveTypes { get; set; }
     }
 }
