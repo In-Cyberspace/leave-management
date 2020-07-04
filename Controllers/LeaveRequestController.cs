c4f861027110d77555031751a01139d07292a1ff
Hugo Molaudzi <57953645+hugo-k-m@users.noreply.github.com>
2020-07-04 15:54:49 +0200

    LeaveRequestController: Scaffolded the leave request controller and implemented dependency injections in the controller. Implemented the "Index" method in the controller.
    LeaveRequestViewModel: "Created the administrator leave request view"
    view model.

---
+++ Controllers/LeaveRequestController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using leave_management.Contracts;
+using leave_management.Data;
+using leave_management.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace leave_management.Controllers
+{
+    [Authorize]
+    public class LeaveRequestController : Controller
+    {
+        private readonly ILeaveRequestRepository _leaveRequestRepo;
+        private readonly IMapper _mapper;
+        private readonly UserManager<Employee> _userManager;
+
+        public LeaveRequestController(
+            ILeaveRequestRepository leaveRequestRepo,
+            IMapper mapper,
+            UserManager<Employee> userManager
+        )
+        {
+            _leaveRequestRepo = leaveRequestRepo;
+            _mapper = mapper;
+            _userManager = userManager;
+        }
+
+        [Authorize(Roles = "Administrator")]
+        // GET: LeaveRequestController
+        public ActionResult Index()
+        {
+            ICollection<LeaveRequest> leaveRequests = _leaveRequestRepo.FindAll();
+            
+            List<LeaveRequestViewModel> leaveRequestModel = _mapper
+            .Map<List<LeaveRequestViewModel>>(leaveRequests);
+
+            AdminLeaveRequestViewViewModel model =
+            new AdminLeaveRequestViewViewModel
+            {
+                ApprovedRequests = leaveRequestModel
+                .Count(q => q.Approved == true),
+
+                PendingRequests = leaveRequestModel
+                .Count(q => q.Approved == null),
+
+                RejectedRequests = leaveRequestModel
+                .Count(q => q.Approved == false),
+
+                TotalRequests = leaveRequestModel.Count,
+
+                LeaveRequests = leaveRequestModel
+            };
+
+            return View(model);
+        }
+
+        // GET: LeaveRequestController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: LeaveRequestController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LeaveRequestController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LeaveRequestController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: LeaveRequestController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LeaveRequestController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: LeaveRequestController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
